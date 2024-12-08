6f9eb5160617833712831d30a9779a64db2446eb
little-vagrant-cc <1850278638@qq.com>
2024-12-08 16:47:11 +0800

    111

---
+++ src/views/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="app-container home">
+   首页
+  </div>
+</template>
+
+<script setup name="Index">
+
+</script>
+
+<style scoped lang="scss">
+.home {
+}
+</style>
+
---
+++ src/views/redirect/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>11111111</div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute();
+const router = useRouter();
+const { params, query } = route
+const { path } = params
+
+router.replace({ path: '/' + path, query })
+</script>
